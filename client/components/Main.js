80dfd6239b44280429d921f26d3450e3a07b92d6
David <davidgcastro93@gmail.com>
2018-11-27 15:51:20 -0500

    Created and adjusted signup component and styling

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
-import Form from './MyForm';
 import Login from '../screens/Login';
+import Signup from '../screens/Signup';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const Main = () => {
@@ -13,7 +13,11 @@ const Main = () => {
           <Nav />
           <div className="spacer" />
           {/*Content goes here*/}
-          <Login />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+
+          </Switch>
         </div>
         {/*Footer goes here*/}
       </div>
