0e05ffcbf6e97d119c993aca6ee3426a4ff2aaed
David <davidgcastro93@gmail.com>
2018-12-03 22:17:46 -0500

    Global, home,  profile styling changes

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,9 +3,6 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home--container">
-      <div class="pickgradient">
-        <img className="home--headerImage" src="/assets/images/hero-1.png" />
-      </div>
       <span className="text--large--light">The Dating App Made For Dating</span>
     </div>
   );
