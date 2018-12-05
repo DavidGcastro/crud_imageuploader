5c913541312ea72d79a976d04e8d7749fe9fef69
David <davidgcastro93@gmail.com>
2018-12-05 14:47:05 -0500

    removed comments

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,24 +5,11 @@ const question = require('./question');
 const answer = require('./answer');
 const photo = require('./photo');
 
-// const questionsSelected = db.define(
-//   'questionsSelected',
-//   {},
-//   { timestamps: false, freezeTableName: true }
-// );
-
 photo.belongsTo(user);
 user.hasMany(photo);
 user.belongsToMany(question, { through: answer });
 question.belongsToMany(user, { through: answer });
 question.hasMany(answer);
 answer.belongsTo(question);
-
-// user.hasMany(answer);
-// answer.belongsTo(user);
-// answer.belongsTo(question);
-
-// user.belongsToMany(question, { through: questionsSelected });
-// question.belongsToMany(user, { through: questionsSelected });
 
 module.exports = { db, user, photo, question, answer };
